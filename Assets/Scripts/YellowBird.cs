307da8468a7951acf9523b842da85dd4ba642721
Trivita Annisa Utami <trivita1900018074@webmail.uad.ac.id>
2021-09-22 01:54:56 +0700

    Main Scene

---
+++ Assets/Scripts/YellowBird.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowBird : Bird
+{
+    [SerializeField] public float _boostForce = 100;
+    public bool _hasBoost = false;
+
+    public void Boost()
+    {
+        if(State == BirdState.Thrown && !_hasBoost)
+        {
+            RigidBody.AddForce(RigidBody.velocity * _boostForce);
+            _hasBoost = true;
+        }
+    }
+
+    public override void OnTap()
+    {
+        Boost();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
